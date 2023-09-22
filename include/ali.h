d02e7baf3e0e26dab1d8e841fa72c8cab818d177
Brian B <brianjbillman@gmail.com>
2023-09-21 22:53:42 -0400

    create function ali_set_value_u64 for setting the value of an ali to a u64 (untested)

--- include/ali.h
+++ include/ali.h
@@ -2,14 +2,15 @@
 #define ALI_H
 
 #include <stddef.h>
+#include <stdint.h>
 
 struct ali {
     size_t * _number;
     size_t _len;
 };
 
-void ali_init(struct ali * self);
-
-void ali_deinit(struct ali * self);
+void ali_init(struct ali *self);
+void ali_deinit(struct ali *self);
+void ali_set_value_u64(struct ali *self, uint64_t val);
 
 #endif // ALI_H
--- src/ali.c
+++ src/ali.c
@@ -1,11 +1,41 @@
 #include "ali.h"
 #include <stdlib.h>
 
-void ali_init(struct ali * self) {
+#define RATIO_PRIMITIVE_TO_SIZE_T_RU(primitive) \
+    ((sizeof(primitive) / sizeof(size_t)) ? (sizeof(primitive) / sizeof(size_t)) : 1)
+
+void ali_init(struct ali *self) {
     self->_number = NULL;
     self->_len = 0;
 }
 
-void ali_deinit(struct ali * self) {
+void ali_deinit(struct ali *self) {
     free(self->_number);
+}
+
+#define RATIO_U64 RATIO_PRIMITIVE_TO_SIZE_T_RU(uint64_t)
+void ali_set_value_u64(struct ali *self, uint64_t val) {
+    if (self->_number == NULL) {
+        self->_number = malloc(sizeof(size_t) * RATIO_U64);
+        if (self->_number == NULL) {
+            // TODO: this function errored due to a failed malloc call; this should eventually return a value that the calling function can check
+            return;
+        }
+    } else {
+        void *tmp = realloc(self->_number, sizeof(size_t) * RATIO_U64);
+        if (tmp == NULL) {
+            // realloc fail; try malloc
+            free(self->_number);
+            self->_number = malloc(sizeof(size_t) * RATIO_U64);
+            if (self->_number == NULL) {
+                // TODO: add error value for return
+                return;
+            }
+        }
+    }
+    self->_len = RATIO_U64;
+    for (size_t i = 0; i < RATIO_U64; i++) {
+        // reverse index access; set to the value shifted properly and casted (potentially down) to a size_t
+        self->_number[RATIO_U64 - 1 - i] = (size_t) (val >> (8 * i * RATIO_U64));
+    }
 }
